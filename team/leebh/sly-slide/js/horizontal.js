a1867f3b4d7be91f95c2881c20eef97716388645
전성환 <ssuhosin@naver.com>
2014-03-19 13:12:31 +0900

    f

--- team/leebh/sly-slide/js/horizontal.js
+++ team/leebh/sly-slide/js/horizontal.js
@@ -8,8 +8,29 @@ jQuery(function($){
 		var frame = $('#effects1');
 		var wrap  = frame.parent();
 
+		console.log(frame.parent());
 		// Call Sly on frame
-		frame.sly({
+//		frame.sly({
+//			horizontal: 1,
+//			itemNav: 'forceCentered',
+//			smart: 1,
+//			activateMiddle: 1,
+//			activateOn: 'click',
+//			mouseDragging: 1,
+//			touchDragging: 1,
+//			releaseSwing: 1,
+//			startAt: 3,
+//			scrollBar: wrap.find('.scrollbar'),
+//			scrollBy: 1,
+//			speed: 300,
+//			elasticBounds: 1,
+//			easing: 'swing',
+//			dragHandle: 1,
+//			dynamicHandle: 1,
+//			clickBar: 1,
+//
+//		});
+		var sly = new Sly(frame, {
 			horizontal: 1,
 			itemNav: 'forceCentered',
 			smart: 1,
@@ -28,56 +49,22 @@ jQuery(function($){
 			dynamicHandle: 1,
 			clickBar: 1,
 
-		});
+		}).init();		
+		
+		for(var i = 0; i < 10; ++i) {
+			sly.add('<li>' + '<img src="http://www.himagemaking.com/data/cheditor4/1311/4eee615c7250e636168c2e9f6b797e0a_tv36IcSE.jpg">' + '</li>',0);
+		}
+		//console.log(sly);
 		
 		// Add item
 		wrap.find('.add').on('click', function () {
-			frame.sly('add', '<li>' + '<img src="http://www.himagemaking.com/data/cheditor4/1311/4eee615c7250e636168c2e9f6b797e0a_tv36IcSE.jpg">' + '</li>');
+			sly.add('<li>' + '<img src="http://www.himagemaking.com/data/cheditor4/1311/4eee615c7250e636168c2e9f6b797e0a_tv36IcSE.jpg">' + '</li>',0);
 		});
 
 		// Remove item
 		wrap.find('.remove').on('click', function () {
-			frame.sly('remove', -1);
+			sly.remove(0);
 		});
 		
 	}());
-	
-	(function () {
-		var frame = $('#effects2');
-		var wrap  = frame.parent();
-
-		// Call Sly on frame
-		frame.sly({
-			horizontal: 1,
-			itemNav: 'forceCentered',
-			smart: 1,
-			activateMiddle: 1,
-			activateOn: 'click',
-			mouseDragging: 1,
-			touchDragging: 1,
-			releaseSwing: 1,
-			startAt: 3,
-			scrollBar: wrap.find('.scrollbar'),
-			scrollBy: 1,
-			speed: 300,
-			elasticBounds: 1,
-			easing: 'swing',
-			dragHandle: 1,
-			dynamicHandle: 1,
-			clickBar: 1,
-
-		});
-		
-		// Add item
-		wrap.find('.add').on('click', function () {
-			frame.sly('add', '<li>' + '<img src="http://www.himagemaking.com/data/cheditor4/1311/4eee615c7250e636168c2e9f6b797e0a_tv36IcSE.jpg">' + '</li>');
-		});
-
-		// Remove item
-		wrap.find('.remove').on('click', function () {
-			frame.sly('remove', -1);
-		});
-		
-	}());
-
 });
